540dacba609820ce15b1e735a389b33de5f6e72b
sachin <112502093+Mrx-Sachin@users.noreply.github.com>
2024-08-11 03:36:10 +0530

    using gnome-screenshot insted of scrot

--- Grabit.sh
+++ Grabit.sh
@@ -4,19 +4,17 @@ set -e
 # Check dependencies
 command -v tesseract >/dev/null 2>&1 || { echo >&2 "Tesseract OCR is required but not installed. Aborting."; exit 1; }
 command -v convert >/dev/null 2>&1 || { echo >&2 "ImageMagick is required but not installed. Aborting."; exit 1; }
-command -v scrot >/dev/null 2>&1 || { echo >&2 "scrot is required but not installed. Aborting."; exit 1; }
+command -v gnome-screenshot >/dev/null 2>&1 || { echo >&2 "gnome-screenshot is required but not installed. Aborting."; exit 1; }
 command -v xclip >/dev/null 2>&1 || { echo >&2 "xclip is required but not installed. Aborting."; exit 1; }
 
 # Language selection menu
 tesseract_lang="eng"
 
-
 # Create temporary directory
 temp_dir=$(mktemp -d)
 
 # Capture screenshot
-# scrot  -s "$temp_dir/screenshot.png" -q 100 -dpi 300
-scrot -s -f  "$temp_dir/screenshot.png" -q 100 -pi 
+gnome-screenshot -a -f "$temp_dir/screenshot.png"
 
 # Improve image quality
 convert -modulate 100,0 -resize 400% "$temp_dir/screenshot.png" "$temp_dir/processed.png"
